cb0dab9969938b2708293618963d1ea9ec79c542
MaramDiab <71607237+MaramDiab@users.noreply.github.com>
2021-06-16 23:06:48 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+//set db connection
+$db_user ='root';
+$db_password ='';
+$db_name ='phprest';
+$db = new PDO('mysl:host=127.0.0.1,;db_name='.$db_name.';charset=utf8',$db_user,$db_password);
+
+
+?>
